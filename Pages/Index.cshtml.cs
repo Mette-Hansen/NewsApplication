b85f1b20dc0fb05db913e106338412b20cc3aa9c
Mette Hansen <meha419@student.sdu.dk>
2023-01-11 14:27:00 +0100

    RSS feed visible on main page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace NewsApplication.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; private set; }
+
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,7 +22,34 @@ namespace NewsApplication.Pages
 
         public void OnGet()
         {
+            XmlDocument rssXmlDoc = new XmlDocument();
 
-        }
+            // Load the RSS file from the RSS URL
+            rssXmlDoc.Load("https://feeds.services.tv2.dk/api/feeds/nyheder/rss");
+
+            // Parse the Items in the RSS file
+            XmlNodeList rssNodes = rssXmlDoc.SelectNodes("rss/channel/item");
+
+            StringBuilder rssContent = new StringBuilder();
+
+            // Iterate through the items in the RSS file
+            foreach (XmlNode rssNode in rssNodes)
+            {
+                XmlNode rssSubNode = rssNode.SelectSingleNode("title");
+                string title = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("link");
+                string link = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssSubNode = rssNode.SelectSingleNode("description");
+                string description = rssSubNode != null ? rssSubNode.InnerText : "";
+
+                rssContent.Append("<a href='" + link + "'>" + title + "</a><br>" + description);
+            }
+            string content = rssContent.ToString();
+            string output = JsonConvert.SerializeObject(content);
+            Message += output;
+        }      
+        
     }
 }
